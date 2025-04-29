590c51fb52ee647d512d899b4e96f5841940b486
psbabia <abhiprakash1979@gmail.com>
2025-04-29 12:17:15 +0530

    Add files via upload

---
+++ Assign.py
@@ -0,0 +1,12 @@
+a = input('Enter the First Number : ')
+b =  input('Enter the Second Number : ')
+a= float(a) # convert from string to float datatype
+b = float(b)
+c= a + b
+print ('Addition :  ',c)
+c= a - b
+print ('Substraction : ',c)
+c= a * b
+print (f'Multiplication : ',c)
+print (f'Division : ',c)
+
---
+++ Welcome.py
@@ -0,0 +1,4 @@
+a = input("Enter your First Name : ")
+b = input("Enter your Second Name : ")
+
+print ("Hello, " , a , " " , b , " welcome to the python program")
